e2e6f9a28e870bf48c1b76bc621346668d0b5134
Dustin <dyelland@gmail.com>
2017-08-14 15:14:37 -0500

    functions pass all of my tests

--- lab-dustin/lib/arithmetic.js
+++ lab-dustin/lib/arithmetic.js
@@ -5,4 +5,12 @@ function addFunction(num1,num2) {
   return num1 + num2;
 }
 
+function subFunction(num1,num2) {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+    return null;
+  }
+  return num1 - num2;
+}
+
 exports.addFunction = addFunction;
+exports.subFunction = subFunction;
