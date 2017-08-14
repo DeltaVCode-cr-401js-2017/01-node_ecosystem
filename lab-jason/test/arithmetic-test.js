1109a3cc2ec125bc9d3a9e9ea2a44b0ae2eefddc
nerdbetter <jasontlogan@gmail.com>
2017-08-14 15:32:10 -0500

    checks for return to be number and refactored const variable for chai

--- lab-jason/test/arithmetic-test.js
+++ lab-jason/test/arithmetic-test.js
@@ -1,25 +1,19 @@
 const assert = require('assert');
-const should = require('chai').should();
+const {expect} = require('chai');
 const arithmetic = require('../lib/arithmetic');
 
 describe('arithmetic', function(){
   describe('#add', function(){
     it('should have an number as a variable', function (){
-      let x = 1;
-      let y = 1;
-      x.should.be.a('number');
-      y.should.be.a('number');
+      expect(arithmetic.add(10,1)).to.be.a('number');
     });
     it('should math correctly', function(){
       assert.strictEqual(arithmetic.add(1,2), 3);
     });
   });
   describe('#sub', function(){
-    it('should have an number as a variable', function (){
-      let x = 1;
-      let y = 1;
-      x.should.be.a('number');
-      y.should.be.a('number');
+    it('should return an number', function (){
+      expect(arithmetic.sub(10,1)).to.be.a('number');
     });
     it('should math correctly', function(){
       assert.strictEqual(arithmetic.sub(10,1), 9);
