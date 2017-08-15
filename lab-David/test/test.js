a3ec00324643d26bac674a139ea3a60d6f0be455
Meepedy <david@psyon.org>
2017-08-14 23:57:38 -0500

    added tests for add function

--- lab-David/test/test.js
+++ lab-David/test/test.js
@@ -6,5 +6,21 @@ describe('Greet Function', function() {
     it('should return \'hello world! when passed \'world!\'', function (){
       assert.strictEqual(greet.greetUser('world!'), 'hello world!');
     });
+    it('should return null when not passed a string', function() {
+      assert.strictEqual(greet.greetUser(12345765), null);
+    });
+  });
+});
+
+const operators = require('../lib/arithmetic.js');
+
+describe('Operator Functions', function(){
+  describe('#add', function(){
+    it('should return 2 when passed params 1, 1', function(){
+      assert.strictEqual(operators.add(1,1), 2);
+    });
+    it('should return null if passed non-numbers', function(){
+      assert.equal(operators.add('1', 1), null);
+    });
   });
 });
