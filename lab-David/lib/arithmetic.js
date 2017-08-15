d6101a163a16b3a7237470775295f2878012db90
Meepedy <david@psyon.org>
2017-08-14 23:14:50 -0500

    added sub function

--- lab-David/lib/arithmetic.js
+++ lab-David/lib/arithmetic.js
@@ -8,4 +8,15 @@ function add (a, b) {
   }
 }
 
+function sub (a, b) {
+  if (typeof a !== 'number') {
+    if(typeof b !== 'number') {
+      return null;
+    }
+  } else {
+    return a - b;
+  }
+}
+
 exports.add = add;
+exports.sub = sub;
